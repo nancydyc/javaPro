c21c14993b259f2a1bbdcb0b1f422ad527de6a6a
Yichen <msdaiyichen@gmail.com>
2021-06-15 22:26:33 -0700

    Add files to review Java syntax

---
+++ arrayPro.java
@@ -0,0 +1,9 @@
+class arrayPro {
+    public static void main(String[] args) {
+        int[] a = new int[5];
+        a[0] = 1;
+        System.out.println(a[0]);
+        System.out.println(a[1]);
+        System.out.println(a.length);
+    }
+}
